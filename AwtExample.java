237eaaef942573e2c6ee9f1aa9a9c3f7604450fe
nengkya <nengkya@yahoo.com>
2023-11-11 16:10:13 +0700

    import java.awt.Frame;

---
+++ AwtExample.java
@@ -0,0 +1,11 @@
+import java.awt.Frame;
+import java.awt.Button;
+
+public class AwtExample extends Frame {
+    public static void main(String[] arguments) {
+	Button button = new Button("HaGa");
+
+
+
+    }
+}
